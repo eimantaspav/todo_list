bea3dedc4d8b98ea497a1149e7a3255f578a428a
eimantaspav <eimantaspav@gmail.com>
2022-09-04 23:07:51 +0300

    build add and remove functionality

--- todo_list/app.js
+++ todo_list/app.js
@@ -1,7 +1,30 @@
 const addForm = document.querySelector(".add");
+const list = document.querySelector(".todos");
 
+const generateTemplate = (todo) => {
+  const html = ` <li class="list-group-item">
+          <span>${todo}</span>
+                    <i class="uil uil-trash-alt delete"></i>
+
+        </li>`;
+
+  list.innerHTML += html;
+};
+
+// add new
 addForm.addEventListener("submit", (e) => {
   e.preventDefault();
   // get user input and trim white space
-  const todo = form.add.value.trim();
+  const todo = addForm.add.value.trim();
+  if (todo.length > 0) {
+    generateTemplate(todo);
+    addForm.reset();
+  }
+});
+
+// delete
+list.addEventListener("click", (e) => {
+  if (e.target.classList.contains("delete")) {
+    e.target.parentElement.remove();
+  }
 });
