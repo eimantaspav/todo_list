6fad6586423afe861d8e416340a0640286f3be1c
eimantaspav <eimantaspav@gmail.com>
2022-09-04 22:33:59 +0300

    build html + css

---
+++ todo_list/app.js
@@ -0,0 +1,7 @@
+const addForm = document.querySelector(".add");
+
+addForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  // get user input and trim white space
+  const todo = form.add.value.trim();
+});
